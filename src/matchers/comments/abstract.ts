af13bca8f9be3cf8055b3687b2ae8fe411820943
Jamshed Javed <jamshady@gmail.com>
2022-11-14 23:54:52 +0000

    Comment Matcher: HTML

--- src/matchers/comments/abstract.ts
+++ src/matchers/comments/abstract.ts
@@ -31,6 +31,7 @@ export default abstract class {
                     const [full, hint] = parts
                     return {
                         full,
+                        curr: hint,
                         hint,
                     }
                 }
