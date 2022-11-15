5b376613eb8e58fe76dd52d71cf9d198c759e1d6
Jamshed Javed <jamshady@gmail.com>
2022-11-15 00:15:40 +0000

    Comment Matcher: Block

---
+++ src/matchers/comments/block.ts
@@ -0,0 +1,16 @@
+import base from './multiline'
+
+/**
+ * Matches HTML comments, i.e. <!-- these ones -->
+ */
+export default class extends base implements Matcher {
+
+    public matches(sourceCode: SourceCode, keywords: Keywords) {
+        return super.matches(
+            sourceCode, keywords,
+            '/\\*+', '\\*+/',
+            line => line.replace(/^\s*\*+/, '')
+        )
+    }
+
+}
