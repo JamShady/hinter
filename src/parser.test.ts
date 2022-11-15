3ad8cf608d0c87a8e52e56e4d15ebd19d80755b4
Jamshed Javed <jamshady@gmail.com>
2022-11-15 02:15:36 +0000

    Hints: Expand Nested

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -10,7 +10,8 @@ describe('Hint Parser Tests', () => {
     const test = (hint: string, expected: string[]) => () => expect(
         (new Parser)
             .parse(hint)
-    ).to.deep.equal(expected)
+            .sort()
+    ).to.deep.equal(expected.sort())
 
 
     it('Tests Raw Hint Passing', test('red blue', ['red','blue']))
@@ -18,5 +19,11 @@ describe('Hint Parser Tests', () => {
     it('Tests Single Expansion', test('foo-(bip|bap)-bar', ['foo-bip-bar','foo-bap-bar']))
 
     it('Tests Multi Expansion', test('(one|two)-(a|b)', ['one-a','one-b','two-a','two-b']))
+
+    it('Tests Nested Expansion', test('foo-(bip|ba(p|r))-boo', [
+        'foo-bip-boo',
+        'foo-bap-boo',
+        'foo-bar-boo',
+    ]))
 
 })
